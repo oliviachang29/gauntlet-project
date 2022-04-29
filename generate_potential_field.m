ec5b44ab15939b78c86affd6c66e07a07b34ee69
Anthony Costarelli <acostarelli@olin.edu>
2022-04-29 18:27:59 -0400

    gen pot field takes inliers

--- generate_potential_field.m
+++ generate_potential_field.m
@@ -1,4 +1,4 @@
-function [potential_field] = generate_potential_field(model, potential_field)
+function [potential_field] = generate_potential_field(inliers, potential_field)
 % GENERATE_POTENTIAL_FIELD Generate and upate the potential field from
 % RANSAC data
 %   [potential_field] = generate_potential_field(model, potential_field)
@@ -6,8 +6,6 @@ function [potential_field] = generate_potential_field(model, potential_field)
 %   Initialize potential_field = 0.
 
 syms x y
-
-inliers = [];
 
 for i=1:length(inliers)
     potential_field = potential_field + ln(sqrt((x-inliers(1))^2 + (y-inliers(2))^2));
