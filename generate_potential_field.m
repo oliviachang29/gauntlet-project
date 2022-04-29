725ed37fe894e8029994c9c8171afb6b3b5173bb
Anthony Costarelli <acostarelli@olin.edu>
2022-04-29 12:53:53 -0400

    initial generate_potential_field

---
+++ generate_potential_field.m
@@ -0,0 +1,16 @@
+function [potential_field] = generate_potential_field(model, potential_field)
+% GENERATE_POTENTIAL_FIELD Generate and upate the potential field from
+% RANSAC data
+%   [potential_field] = generate_potential_field(model, potential_field)
+%   updates the pontential_field with the model output from RANSAC.
+%   Initialize potential_field = 0.
+
+syms x y
+
+inliers = [];
+
+for i=1:length(inliers)
+    potential_field = potential_field + ln(sqrt((x-inliers(1))^2 + (y-inliers(2))^2));
+end
+
+end
