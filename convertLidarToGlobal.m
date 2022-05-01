8cc97f96a186c5ca03d3d9046d71f3587371301c
Olivia Chang <ochang@olin.edu>
2022-05-01 19:09:35 -0400

    fix: take only first 2 rows of g frame points

--- convertLidarToGlobal.m
+++ convertLidarToGlobal.m
@@ -10,27 +10,27 @@ function [points] = convertLidarToGlobal(r_lidar, theta_lidar, neato_pos, neato_
     % the origin of the Lidar frame in the Neato frame (ihat_N, jhat_N).
     origin_of_lidar_frame = [-0.084 0];
     
-    cartesianPointsInLFrame = [cos(theta_lidar).*r_lidar...
+    cartesian_points_in_l_frame = [cos(theta_lidar).*r_lidar...
                                sin(theta_lidar).*r_lidar]';
-                           
+      
     % add a row of all ones so we can use translation matrices
-    cartesianPointsInLFrame(end+1,:) = 1;
-    cartesianPointsInNFrame = [1 0 origin_of_lidar_frame(1);...
+    cartesian_points_in_l_frame(end+1,:) = 1;
+    cartesian_points_in_n_frame = [1 0 origin_of_lidar_frame(1);...
                                0 1 origin_of_lidar_frame(2);...
-                               0 0 1]*cartesianPointsInLFrame;
-
+                               0 0 1]*cartesian_points_in_l_frame;
+                           
     % undo the rotation of the Neato so the points are with respected to
     % the global coordinate axes with the origin located at the Neato's
     % position
-    rotatedPoints = [cos(neato_orientation) -sin(neato_orientation) 0;...
+    rotated_points = [cos(neato_orientation) -sin(neato_orientation) 0;...
                      sin(neato_orientation) cos(neato_orientation) 0;...
-                     0 0 1]*cartesianPointsInNFrame;
+                     0 0 1]*cartesian_points_in_n_frame;
                  
     % translate the points to be relative to the Global origin
-    cartesianPointsInGFrame = [1 0 neato_pos(1);...
+    cartesian_points_in_g_frame = [1 0 neato_pos(1);...
                                0 1 neato_pos(2);...
-                               0 0 1]*rotatedPoints;
-                           
-    points = cartesianPointsInGFrame;
+                               0 0 1]*rotated_points;
+                                                
+    points = [cartesian_points_in_g_frame(1,:); cartesian_points_in_g_frame(2,:)]';
     
 end
