51f8aa0a9c300fc568cdf273f65d2223319941ad
Anthony Costarelli <acostarelli@olin.edu>
2022-05-01 21:45:21 -0400

    drive nows rotates and drives... unsure if lambda should be hardcoded, an argument, or if caller should modify v itself

--- drive.m
+++ drive.m
@@ -1,21 +1,37 @@
-function drive(v)
-% DRIVE Drive the Neato forwardv
-%   drive(v) Drives for the length of v
+function [position, heading] = drive(v, position, heading)
 
+angular_speed = 0.2;
 linear_speed  = 0.75;
+wheel_base    = 0.235;
+lambda        = 0.1;
 
-% Drive forward for a bit
-dist = norm(grad);
-time = dist / linear_speed;
-msg.Data = [linear_speed, linear_speed];
+pub = rospublisher('/raw_vel');
+msg = rosmessage(pub);
+
+prod  = cross([heading 0], [v 0]);
+dir   = sign(prod);
+angle = asin(norm(prod) / norm(heading) * norm(v));
+time  = double(angle) / angular_speed;
+msg.Data = [-dir*angular_speed*wheel_base/2 dir*angular_speed*wheel_base/2];
 send(pub, msg);
 start = rostic;
 while rostoc(start) < time
     pause(0.01);
 end
 
-% Stop
+dist = norm(v) * lambda;
+time = dist / linear_speed;
+msg.Data = [linear_speed linear_speed];
+send(pub, msg);
+start = rostic;
+while rostoc(start) < time
+    pause(0.01);
+end
+
 msg.Data = [0 0];
 send(msg, pub)
+
+position = position + v*lambda;
+heading  = v;
 
 end
